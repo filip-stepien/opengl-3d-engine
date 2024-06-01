a2a9f90631752fa7a280901daeb36e3dcf3c6f1f
Filip Stępień <filip.stepien30@gmail.com>
2024-06-01 15:25:00 +0200

    drawable & ebo docs

--- src/include/ElementBuffer.hpp
+++ src/include/ElementBuffer.hpp
@@ -5,17 +5,44 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 
+/**
+ * @class ElementBuffer
+ * @brief A class representing an OpenGL Element Buffer Object (EBO).
+ */
 class ElementBuffer {
 private:
-	GLuint id;
+    /// ID of the element buffer.
+    GLuint id;
 
 public:
-	ElementBuffer();
-	~ElementBuffer();
+    /**
+     * @brief Constructor for ElementBuffer.
+     * Generates and initializes the element buffer.
+     */
+    ElementBuffer();
 
-	void setData(std::vector<GLuint>& indices, GLenum usage = GL_STATIC_DRAW);
-	void bind();
-	void unbind();
+    /**
+     * @brief Destructor for ElementBuffer.
+     * Deletes the element buffer.
+     */
+    ~ElementBuffer();
+
+    /**
+     * @brief Sets the data for the element buffer.
+     * @param indices A vector of indices to be stored in the buffer.
+     * @param usage The usage pattern of the buffer. Defaults to GL_STATIC_DRAW.
+     */
+    void setData(std::vector<GLuint>& indices, GLenum usage = GL_STATIC_DRAW);
+
+    /**
+     * @brief Binds the element buffer.
+     */
+    void bind();
+
+    /**
+     * @brief Unbinds the element buffer.
+     */
+    void unbind();
 };
 
 #endif
