76242b265b0bb42a5e93d7a90bc74ab12933093a
Filip Stępień <filip.stepien30@gmail.com>
2024-06-01 19:07:53 +0200

    mesh & plane docs

--- src/include/Plane3D.hpp
+++ src/include/Plane3D.hpp
@@ -3,12 +3,29 @@
 
 #include "Mesh.hpp"
 
+/**
+ * @class Plane3D
+ * @brief Plane mesh.
+ */
 class Plane3D : public Mesh {
 public:
+    /**
+     * @brief Constructor for Plane3D.
+     * Initializes the mesh with vertices and indices for a 3D plane.
+     */
     Plane3D() : Mesh(getVertices(), getIndices()) {}
 
 private:
+    /**
+     * @brief Gets the vertices for the 3D plane.
+     * @return A vector of vertices for the 3D plane.
+     */
     std::vector<Vertex> getVertices();
+
+    /**
+     * @brief Gets the indices for the 3D plane.
+     * @return A vector of indices for the 3D plane.
+     */
     std::vector<GLuint> getIndices();
 };
 
