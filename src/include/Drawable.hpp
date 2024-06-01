830630fd4722cb5b69cea3f2ec7e54ebf19968b7
Filip Stępień <filip.stepien30@gmail.com>
2024-06-01 15:25:00 +0200

    drawable & ebo docs

--- src/include/Drawable.hpp
+++ src/include/Drawable.hpp
@@ -3,9 +3,22 @@
 
 #include "Shader.hpp"
 
+/**
+ * @class Drawable
+ * @brief An abstract class representing a drawable object.
+ */
 class Drawable {
 public:
-	virtual void draw(Shader& shader) {};
+    /**
+     * @brief Draws the object using the provided shader.
+     * @param shader The shader to be used for drawing.
+     */
+    virtual void draw(Shader& shader) {};
+
+    /**
+     * @brief Draws the object to a buffer using the provided shader.
+     * @param shader The shader to be used for drawing to the buffer.
+     */
     virtual void drawToBuffer(Shader& shader) {};
 };
 
